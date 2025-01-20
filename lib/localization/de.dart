0beedca9933d9553679a85440ba99cb5eecee1d2
BeRe197 <benjamin@reiser-family.de>
2025-01-20 20:30:15 +0100

    dart format

--- lib/localization/de.dart
+++ lib/localization/de.dart
@@ -3,25 +3,25 @@ import 'package:duration_picker/localization/localization.dart';
 class DurationPickerLocalizationsDe extends DurationPickerLocalizations {
   @override
   String get baseUnitHour => 'Std.';
-  
+
   @override
   String get baseUnitMillisecond => 'ms';
-  
+
   @override
   String get baseUnitMinute => 'Min.';
-  
+
   @override
   String get baseUnitSecond => 'Sek.';
-  
+
   @override
   String get secondaryUnitHour => 'T ';
-  
+
   @override
   String get secondaryUnitMillisecond => 'S ';
-  
+
   @override
   String get secondaryUnitMinute => 'Std. ';
-  
+
   @override
   String get secondaryUnitSecond => 'Min. ';
 }
